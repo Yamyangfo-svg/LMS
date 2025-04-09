95623ac455d18d66fd47d836b79254eccd5d0367
shristikachettri <157007597+shristikachettri@users.noreply.github.com>
2025-04-09 06:57:35 +0530

    Resolved merge conflict in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+<<<<<<< Updated upstream
 import Authors from "./Components/User Management/Authors";
+=======
+import LanguageList from "./pages/LanguageList";
+import book from "./pages/book";
+
+
+>>>>>>> Stashed changes
 import "./App.css";
 
 function App() {
@@ -11,7 +18,13 @@ function App() {
         <Routes>
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
+<<<<<<< Updated upstream
           <Route path="/authors" element={<Authors />} />
+=======
+          <Route path="/LanguageList" element={<LanguageList/>} />
+          <Route path="/book" element={<book/>} />
+          
+>>>>>>> Stashed changes
         </Routes>
       </div>
     </Router>
